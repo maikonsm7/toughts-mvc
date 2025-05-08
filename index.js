c17979775453af22dba5836cf8bb2204503037b9
maikonsm7 <maikonsm7@gmail.com>
2025-05-08 13:50:56 -0400

    create models

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
 require('dotenv').config()
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+// database connect
+const conn = require('./db/conn')
+
+// models
+const Tought = require('./models/Tought')
+const User = require('./models/User')
+
+// engine config
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
+
+// public path
 app.use(express.static('public'))
 
-const conn = require('./db/conn')
+// session config
+app.use(session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+        logFn: function (){},
+        path: require('path').join(require('os').tmpdir(), 'sessions')
+    }),
+    cookie: {
+        secure: false,
+        maxAge: 360000,
+        expires: new Date(Date.now() + 360000),
+        httpOnly: true
+    }
+}))
+
+// flash messages
+app.use(flash())
+
+// set session to response
+app.use((req, res, next)=>{
+    if(req.session.userid){
+        res.locals.session = req.session
+    }
+    next()
+})
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+// conn.sync({force: true})
 conn.sync()
 .then(app.listen(port, ()=>{
     console.log(`http://localhost:${port}`)
